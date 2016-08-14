9685a7e52ddd72007bbf5ea58a4bc818900a21e2
Eric McCarthy <eric@limulus.net>
2016-08-13 23:06:53 -0700

    Convert HTMLParser tests from Jasmine to Chai's expect and Sinon

--- test/HTMLParserTest.js
+++ test/HTMLParserTest.js
@@ -1,4 +1,6 @@
-import {expect} from 'chai'
+import chai, {expect} from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
 import HTMLParser from '../lib/HTMLParser'
 import Instr from '../lib/Instr'
 import Op from '../lib/Op'
@@ -6,6 +8,8 @@ import TagNode from '../lib/Node/TagNode'
 import TextNode from '../lib/Node/TextNode'
 import AttrNode from '../lib/Node/AttrNode'
 import CloseTagNode from '../lib/Node/CloseTagNode'
+
+chai.use(sinonChai)
 
 describe("HTMLParser", function () {
   var parser;
@@ -20,72 +24,72 @@ describe("HTMLParser", function () {
     };
 
     it("should parse the empty string", function () {
-      expect(parse("")).toBe("");
+      expect(parse("")).to.be.equal("");
     })
 
     it("should parse a text node", function () {
-      expect(parse("hi")).toBe("'hi'");
+      expect(parse("hi")).to.be.equal("'hi'");
     })
 
     it("should parse a comment", function () {
-      expect(parse("<!-- hiya -->")).toBe("<!-- hiya -->");
+      expect(parse("<!-- hiya -->")).to.be.equal("<!-- hiya -->");
     })
 
     it("should parse a comment with dashes", function () {
-      expect(parse("<!-- hello--world -->")).toBe("<!-- hello--world -->");
+      expect(parse("<!-- hello--world -->")).to.be.equal("<!-- hello--world -->");
     })
 
     it("should parse a named HTML entity", function () {
-      expect(parse("&gt;")).toBe("&(gt);");
+      expect(parse("&gt;")).to.be.equal("&(gt);");
     })
 
     it("should parse a hex HTML entity", function () {
-      expect(parse("&#x20AC;")).toBe("&(#x20AC);");
+      expect(parse("&#x20AC;")).to.be.equal("&(#x20AC);");
     })
 
     it("should parse a lone ampersand", function () {
-      expect(parse("lone & ampersand")).toBe("'lone ''&'' ampersand'");
+      expect(parse("lone & ampersand")).to.be.equal("'lone ''&'' ampersand'");
     })
 
     it("should parse a lone ampersand inside an attribute value", function () {
       expect(parse('<a href="http://example.com/foo?a=1&b=2"></a>'))
-        .toBe("<a href='http://example.com/foo?a=1&b=2'></a>")
+        .to.be.equal("<a href='http://example.com/foo?a=1&b=2'></a>")
     })
 
     it("should parse a lone >", function () {
-      expect(parse("lone > gt")).toBe("'lone ''>'' gt'");
+      expect(parse("lone > gt")).to.be.equal("'lone ''>'' gt'");
     })
 
     it("should parse a void tag", function () {
-      expect(parse("<br>")).toBe("<br>");
+      expect(parse("<br>")).to.be.equal("<br>");
     })
 
     it("should parse a tag with an attribute delimited by double quotes", function () {
-      expect(parse('<br clear="right">')).toBe("<br clear='right'>");
+      expect(parse('<br clear="right">')).to.be.equal("<br clear='right'>");
     })
 
     it("should parse a tag with two attributes", function () {
-      expect(parse('<br clear="right"  style="foo:bar">')).toBe("<br clear='right' style='foo:bar'>");
+      expect(parse('<br clear="right"  style="foo:bar">')).to.be.equal("<br clear='right' style='foo:bar'>");
     })
 
     it("should parse an empty attribute", function () {
-      expect(parse('<hr prop>')).toBe("<hr prop=''>");
+      expect(parse('<hr prop>')).to.be.equal("<hr prop=''>");
     })
 
     it("should parse a self closing void tag", function () {
-      expect(parse('<hr />')).toBe("<hr>");
+      expect(parse('<hr />')).to.be.equal("<hr>");
     })
 
     it("should parse a lone closing tag", function () {
-      expect(parse('</div>')).toBe("</div>");
+      expect(parse('</div>')).to.be.equal("</div>");
     })
 
     it("should parse a simple div tag", function () {
-      expect(parse('<div>foo</div>')).toBe("<div>'foo'</div>");
+      expect(parse('<div>foo</div>')).to.be.equal("<div>'foo'</div>");
     })
 
     it("should avoid throwing ScannerError on a `&` followed by whitespace", function () {
-      expect(function () { parser.parse("&C\n") }).not.toThrow();
+      expect(function () { parser.parse("&C\n") }).not.to.throw();
     })
   })
 
@@ -93,7 +97,7 @@ describe("HTMLParser", function () {
     var validate = HTMLParser.validate;
 
     it("should consider header tags valid HTML", function () {
-      expect(validate("<h1>foo</h1>")).toEqual([]);
+      expect(validate("<h1>foo</h1>")).to.deep.equal([]);
     })
   })
 
@@ -108,14 +112,14 @@ describe("HTMLParser", function () {
     it("should unfinished nodes from the stack", function () {
       parser.pushNode(tagNode);
       parser.finalize();
-      expect(function () { parser.currentNode() }).toThrow();
-      expect(parser.currentOpenElement()).toBe(parser.parseTree());
+      expect(function () { parser.currentNode() }).to.throw();
+      expect(parser.currentOpenElement()).to.be.equal(parser.parseTree());
     })
 
     it("should move open elements into the parse tree", function () {
       parser.pushOpenElement(tagNode);
       parser.finalize();
-      expect(parser.parseTree().lastChild()).toBe(tagNode);
+      expect(parser.parseTree().lastChild()).to.be.equal(tagNode);
     })
 
     it("should add errors to the topmost unfinished node", function () {
@@ -123,39 +127,39 @@ describe("HTMLParser", function () {
       parser.pushNode(tagNode);
       parser.pushNode(attrNode);
       parser.finalize();
-      expect(tagNode.ownErrors().length).toBe(0);
-      expect(attrNode.ownErrors().length).toBe(1);
+      expect(tagNode.ownErrors().length).to.be.equal(0);
+      expect(attrNode.ownErrors().length).to.be.equal(1);
     })
   })
 
   describe("#executeOp", function () {
     it("should call popNode when passed a POP_NODE op", function () {
-      parser.popNode = spyOnMethods(parser, parser.popNode);
+      sinon.stub(parser, 'popNode');
       var op = new Op(Instr.POP_NODE);
       parser.executeOp(op);
-      expect(parser.popNode).toHaveBeenCalled();
+      expect(parser.popNode).to.have.been.called;
     })
 
     it("should call pushNode when passed a PUSH_NODE op", function () {
-      parser.pushNode = spyOnMethods(parser, parser.pushNode);
+      sinon.spy(parser, 'pushNode');
       var op = new Op(Instr.PUSH_NODE);
       var node = new TextNode();
       op.setNode(node);
       parser.executeOp(op);
-      expect(parser.pushNode).toHaveBeenCalledWith(op.node());
+      expect(parser.pushNode).to.have.been.calledWith(op.node());
     })
 
     it("should call node.addToken when passed a ADD_TOKEN op", function () {
       var node = new TextNode();
-      node.addToken = spyOnMethods(node, node.addToken);
+      sinon.spy(node, 'addToken');
 
       var op = new Op(Instr.ADD_TOKEN);
-      var tok = new Foundation.Scanner.Token("text", "foo", 0, 1, 0);
+      var tok = {type: "text", value: "foo", index: 0, line: 1, column: 0};
       op.setToken(tok);
 
       parser.pushNode(node);
       parser.executeOp(op);
-      expect(node.addToken).toHaveBeenCalledWith(tok);
+      expect(node.addToken).to.have.been.calledWith(tok);
     })
   })
 
@@ -163,11 +167,11 @@ describe("HTMLParser", function () {
     it("should return the node at the top of the node stack", function () {
       var node = new TextNode();
       parser.pushNode(node);
-      expect(parser.currentNode()).toBe(node);
+      expect(parser.currentNode()).to.be.equal(node);
     })
 
     it("should throw an error when the node stack is empty", function () {
-      expect(function () { parser.currentNode() }).toThrow();
+      expect(function () { parser.currentNode() }).to.throw();
     })
   })
 
@@ -178,20 +182,20 @@ describe("HTMLParser", function () {
       parser.pushNode(node1);
       parser.pushNode(node2);
       parser.popNode();
-      expect(parser.currentNode()).toBe(node1);
+      expect(parser.currentNode()).to.be.equal(node1);
     })
 
     it("should call the applyCompletedNode method", function () {
-      parser.applyCompletedNode = spyOnMethods(parser, parser.applyCompletedNode);
+      sinon.spy(parser, 'applyCompletedNode');
 
       var node = new TextNode();
       parser.pushNode(node);
       parser.popNode();
-      expect(parser.applyCompletedNode).toHaveBeenCalledWith(node);
+      expect(parser.applyCompletedNode).to.have.been.calledWith(node);
     })
 
     it("should throw an error if the node stack is empty", function () {
-      expect(function () { parser.popNode() }).toThrow();
+      expect(function () { parser.popNode() }).to.throw();
     })
   })
 
@@ -199,21 +203,21 @@ describe("HTMLParser", function () {
     it("should add a text node to the currently open element", function () {
       var node = new TextNode();
       parser.applyCompletedNode(node);
-      expect(parser.currentOpenElement().children().pop()).toBe(node);
+      expect(parser.currentOpenElement().children().pop()).to.be.equal(node);
     })
 
     it("should add an attribute node to the current tag node", function () {
       var attr = new AttrNode();
       parser.pushNode(new TagNode());
       parser.applyCompletedNode(attr);
-      expect(parser.currentNode().attributes()[0]).toBe(attr);
+      expect(parser.currentNode().attributes()[0]).to.be.equal(attr);
     })
 
     it("should throw an error if it attempts to add an attribute node to a non-tag node", function () {
       parser.pushNode(new TextNode());
       expect(function () {
         parser.applyCompletedNode(new AttrNode());
-      }).toThrow();
+      }).to.throw();
     })
 
     it("should add void tags to the current open element", function () {
@@ -222,26 +226,26 @@ describe("HTMLParser", function () {
       parser.applyCompletedNode(br);
       var openElement = parser.currentOpenElement();
       expect(openElement !== br);
-      expect(openElement.lastChild()).toBe(br);
+      expect(openElement.lastChild()).to.be.equal(br);
     })
 
     it("should push non-void tags onto the open element stack", function () {
       var div = new TagNode();
       div.setTagName("div");
       parser.applyCompletedNode(div);
-      expect(parser.currentOpenElement()).toBe(div);
+      expect(parser.currentOpenElement()).to.be.equal(div);
     })
 
     it("should call mostRecentOpenElementWithName when passed a close tag node", function () {
-      parser.mostRecentOpenElementWithName = spyOnMethods(parser, parser.mostRecentOpenElementWithName);
+      sinon.spy(parser, 'mostRecentOpenElementWithName');
       var close = new CloseTagNode();
       close.setTagName("FOO");
       parser.applyCompletedNode(close);
-      expect(parser.mostRecentOpenElementWithName).toHaveBeenCalledWith("foo");
+      expect(parser.mostRecentOpenElementWithName).to.have.been.calledWith("foo");
     })
 
     it("should call addClosedElementToParent when passed a close tag with a matching open tag", function () {
-      parser.addClosedElementToParent = spyOnMethods(parser, parser.addClosedElementToParent);
+      sinon.spy(parser, 'addClosedElementToParent');
 
       var div = new TagNode();
       div.setTagName("div");
@@ -250,16 +254,16 @@ describe("HTMLParser", function () {
 
       parser.pushOpenElement(div);
       parser.applyCompletedNode(closeDiv);
-      expect(parser.addClosedElementToParent).toHaveBeenCalledWith(div);
+      expect(parser.addClosedElementToParent).to.have.been.calledWith(div);
     })
 
     it("should call setRawtextModeForTag on the compiler object when called with a rawtext tag node", function () {
       var compiler = parser.compiler();
-      compiler.setRawtextModeForTag = spyOnMethods(compiler, compiler.setRawtextModeForTag);
+      sinon.spy(compiler, 'setRawtextModeForTag');
       var script = new TagNode();
       script.setTagName("script");
       parser.applyCompletedNode(script);
-      expect(compiler.setRawtextModeForTag).toHaveBeenCalledWith("script");
+      expect(compiler.setRawtextModeForTag).to.have.been.calledWith("script");
     })
 
     it("should add a closing tag node to the tag node it closes", function () {
@@ -273,7 +277,7 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div);
       parser.pushOpenElement(span);
       parser.applyCompletedNode(divClose);
-      expect(div.closingTag()).toBe(divClose);
+      expect(div.closingTag()).to.be.equal(divClose);
     })
 
     it("should add a closing tag node to the current open element if there is no matching open tag", function () {
@@ -284,7 +288,7 @@ describe("HTMLParser", function () {
 
       parser.pushOpenElement(div);
       parser.applyCompletedNode(spanClose);
-      expect(parser.currentOpenElement().lastChild()).toBe(spanClose);
+      expect(parser.currentOpenElement().lastChild()).to.be.equal(spanClose);
     })
 
     it("should set an error on bogus closing tags", function () {
@@ -295,7 +299,7 @@ describe("HTMLParser", function () {
 
       parser.pushOpenElement(div);
       parser.applyCompletedNode(spanClose);
-      expect(spanClose.ownErrors().length).toBe(1);
+      expect(spanClose.ownErrors().length).to.be.equal(1);
     })
   })
 
@@ -315,7 +319,7 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div1);
       parser.pushOpenElement(div2);
       parser.addClosedElementToParent(div2);
-      expect(parser.currentOpenElement()).toBe(div1);
+      expect(parser.currentOpenElement()).to.be.equal(div1);
     })
 
     it("should make closed elements children of each other and the currently open element", function () {
@@ -328,10 +332,10 @@ describe("HTMLParser", function () {
       div1.appendChild(closeDiv);
       parser.addClosedElementToParent(div1);
 
-      expect(parser.currentOpenElement().lastChild()).toBe(div1);
-      expect(div1.lastChild()).toBe(div2);
-      expect(div2.lastChild()).toBe(div3);
-      expect(div3.lastChild()).toBe(null);
+      expect(parser.currentOpenElement().lastChild()).to.be.equal(div1);
+      expect(div1.lastChild()).to.be.equal(div2);
+      expect(div2.lastChild()).to.be.equal(div3);
+      expect(div3.lastChild()).to.be.equal(null);
     })
 
     it("should set an error on closing tag when it closes other tags still open", function () {
@@ -344,7 +348,7 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div1);
       parser.pushOpenElement(span);
       parser.addClosedElementToParent(div1);
-      expect(closeDiv.ownErrors().length).toBe(1);
+      expect(closeDiv.ownErrors().length).to.be.equal(1);
     })
 
     it("should set an error on elements without closing tags", function () {
@@ -357,9 +361,9 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div3);
       parser.addClosedElementToParent(div1);
 
-      expect(div1.ownErrors().length).toBe(0);
-      expect(div2.ownErrors().length).toBe(1);
-      expect(div3.ownErrors().length).toBe(1);
+      expect(div1.ownErrors().length).to.be.equal(0);
+      expect(div2.ownErrors().length).to.be.equal(1);
+      expect(div3.ownErrors().length).to.be.equal(1);
     })
   })
 
@@ -379,12 +383,12 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div1);
       parser.pushOpenElement(div2);
       parser.popElementsToAndIncluding(div2);
-      expect(parser.currentOpenElement()).toBe(div1);
+      expect(parser.currentOpenElement()).to.be.equal(div1);
     })
 
     it("should throw an error when trying to find an element not on the open element stack", function () {
       parser.pushOpenElement(div1);
-      expect(function () { parser.popElementsToAndIncluding(div2) }).toThrow();
+      expect(function () { parser.popElementsToAndIncluding(div2) }).to.throw();
     })
 
     it("should return the closed elements", function () {
@@ -393,7 +397,7 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div3);
       var closedElements = parser.popElementsToAndIncluding(div1);
 
-      expect(closedElements).toEqual([div3, div2, div1]);
+      expect(closedElements).to.deep.equal([div3, div2, div1]);
     })
   })
 
@@ -410,25 +414,25 @@ describe("HTMLParser", function () {
       parser.pushOpenElement(div2);
       parser.pushOpenElement(p);
 
-      expect(parser.mostRecentOpenElementWithName("div")).toBe(div2);
+      expect(parser.mostRecentOpenElementWithName("div")).to.be.equal(div2);
     })
   })
 
   describe("#currentOpenElement", function () {
     it("should return the root element for a brand new parser", function () {
-      expect(parser.currentOpenElement()).toBe(parser.parseTree());
+      expect(parser.currentOpenElement()).to.be.equal(parser.parseTree());
     })
 
     it("should return the top of the open element stack", function () {
       var node = new TagNode();
       parser.pushOpenElement(node);
-      expect(parser.currentOpenElement()).toBe(node);
+      expect(parser.currentOpenElement()).to.be.equal(node);
     })
   })
 
   describe("#popOpenElement", function () {
     it("should throw an error when trying to pop the root node", function () {
-      expect(function () { parser.popOpenElement() }).toThrow();
+      expect(function () { parser.popOpenElement() }).to.throw();
     })
   })
 })
