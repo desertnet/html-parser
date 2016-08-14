fa814c187a0aa273fd2189cffb31ff878bd7f39c
Eric McCarthy <eric@limulus.net>
2016-08-14 12:18:53 -0700

    Conver Op tests to use Chai

--- test/OpTest.js
+++ test/OpTest.js
@@ -1,3 +1,5 @@
+import {expect} from 'chai'
+import {Token as ScannerToken} from '@desertnet/scanner'
 import Instr from '../lib/Instr'
 import Op from '../lib/Op'
 import TextNode from '../lib/Node/TextNode'
@@ -9,7 +11,7 @@ describe("Op", function () {
       var op = new Op(
         Instr.POP_NODE
       );
-      expect(op.instruction()).toBe(Instr.POP_NODE);
+      expect(op.instruction()).to.be.equal(Instr.POP_NODE);
     })
   })
 
@@ -18,17 +20,17 @@ describe("Op", function () {
       var op = new Op(
         Instr.POP_NODE
       );
-      var tok = new Foundation.Scanner.Token("foo", "foo", 0, 1, 0);
-      expect(function () { op.setToken(tok) }).toThrow();
+      var tok = new ScannerToken("foo", "foo", 0, 1, 0);
+      expect(function () { op.setToken(tok) }).to.throw();
     })
 
     it("should set the token for ADD_TOKEN ops", function () {
       var op = new Op(
         Instr.ADD_TOKEN
       );
-      var tok = new Foundation.Scanner.Token("foo", "foo", 0, 1, 0);
+      var tok = new ScannerToken("foo", "foo", 0, 1, 0);
       op.setToken(tok);
-      expect(op.token()).toBe(tok);
+      expect(op.token()).to.be.equal(tok);
     })
   })
 
@@ -37,7 +39,7 @@ describe("Op", function () {
       var op = new Op(
         Instr.ADD_TOKEN
       );
-      expect(function () { op.token() }).toThrow();
+      expect(function () { op.token() }).to.throw();
     })
   })
 
@@ -48,7 +50,7 @@ describe("Op", function () {
       var op = new Op(
         Instr.POP_NODE
       );
-      expect(function () { op.setNode(node) }).toThrow();
+      expect(function () { op.setNode(node) }).to.throw();
     })
 
     it("should set the node for PUSH_NODE ops", function () {
@@ -56,7 +58,7 @@ describe("Op", function () {
         Instr.PUSH_NODE
       );
       op.setNode(node);
-      expect(op.node()).toBe(node);
+      expect(op.node()).to.be.equal(node);
     })
   })
 
@@ -65,7 +67,7 @@ describe("Op", function () {
       var op = new Op(
         Instr.PUSH_NODE
       );
-      expect(function () { op.node() }).toThrow();
+      expect(function () { op.node() }).to.throw();
     })
   })
 
@@ -73,19 +75,19 @@ describe("Op", function () {
     it("should return a string containing the instr name and node for PUSH_NODE ops", function () {
       var op = new Op(Instr.PUSH_NODE);
       op.setNode(new TextNode());
-      expect(op.toString()).toBe("PUSH_NODE:TEXT");
+      expect(op.toString()).to.be.equal("PUSH_NODE:TEXT");
     })
 
     it("should return a string containing the instr name and token type for ADD_TOKEN ops", function () {
       var op = new Op(Instr.ADD_TOKEN);
-      var token = new Foundation.Scanner.Token("text", "foo", 0, 1, 0);
+      var token = new ScannerToken("text", "foo", 0, 1, 0);
       op.setToken(token);
-      expect(op.toString()).toBe("ADD_TOKEN:text");
+      expect(op.toString()).to.be.equal("ADD_TOKEN:text");
     })
 
     it("should return a string containing just the instr name for POP_NODE ops", function () {
       var op = new Op(Instr.POP_NODE);
-      expect(op.toString()).toBe("POP_NODE");
+      expect(op.toString()).to.be.equal("POP_NODE");
     })
   })
 })
